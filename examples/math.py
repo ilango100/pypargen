64110e0b373d7a5919026e2e87c675832f919224
Ilango Rajagopal <ilangokul@gmail.com>
2020-12-23 19:29:41 +0530

    Added examples math and ab_count

---
+++ examples/math.py
@@ -0,0 +1,51 @@
+"""This example shows how to build a simple desk calculator with parser
+for basic math operations. It supports addition, subtraction, multiplication,
+division and brackets, with proper precedence."""
+
+from pypargen.lr1 import Grammar, Parser
+import sys
+
+math_rules = [("atom", ['"[1-9][0-9]*"']), ("atom", [r'"\("', "sub", r'"\)"']),
+              ("div", ["div", '"/"', "atom"]), ("div", ["atom"]),
+              ("mul", ["mul", r'"\*"', "div"]), ("mul", ["div"]),
+              ("add", ["add", r'"\+"', "mul"]), ("add", ["mul"]),
+              ("sub", ["sub", '"-"', "add"]), ("sub", ["add"]),
+              ("expr", ["sub", r'"\n\n*"'])]
+
+math = Grammar(math_rules, "expr")
+
+
+def convnum(a):
+    return int(a)
+
+
+def bracket(a, b, c):
+    print(a, b, c)
+    return b
+
+
+def div(a, _, b):
+    return a / b
+
+
+def mul(a, _, b):
+    return a * b
+
+
+def add(a, _, b):
+    return a + b
+
+
+def sub(a, _, b):
+    return a - b
+
+
+def nop(a, *_):
+    return a
+
+
+callbacks = [convnum, bracket, div, nop, mul, nop, add, nop, sub, nop, nop]
+parser = Parser(math, callbacks, sys.stdin)
+result = parser.parse()
+
+print(result)
