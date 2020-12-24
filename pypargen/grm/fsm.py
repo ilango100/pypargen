e39abd968e331072d4c274e6b5f69f7b376bfd53
Ilango Rajagopal <ilangokul@gmail.com>
2020-12-25 04:19:04 +0530

    FSM implementations done, for Regexp

---
+++ pypargen/grm/fsm.py
@@ -0,0 +1,101 @@
+# Copyright 2021 Ilango Rajagopal
+# Licensed under GPL-3.0-only
+
+
+class NFANode(dict[str, set["NFANode"]]):
+    _next_id = 1
+
+    def __new__(cls, token=None, *args, **kwargs):
+        node = super().__new__(cls, *args, **kwargs)
+        node._id = cls._next_id
+        cls._next_id += 1
+        if token:
+            node.token = token
+        return node
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def add_transition(self, char: str, nxt: "NFANode"):
+        assert len(char) <= 1,\
+                "Transition can only be added with single or less character"
+        if char in self:
+            self[char].add(nxt)
+            return
+        self[char] = {nxt}
+
+    def __eq__(self, other: "NFANode"):
+        return self._id == other._id
+
+    def __ne__(self, other: "NFANode"):
+        return self._id != other._id
+
+    def __hash__(self):
+        return self._id
+
+
+class NFA:
+    def __init__(self, start: NFANode = None, end: NFANode = None):
+        if start is None:
+            start = NFANode()
+        if end is None:
+            end = NFANode()
+        self.start = start
+        self.end = end
+
+
+class DFAConflict(Exception):
+    def __init__(self, conflicts):
+        super().__init__(f"Conflicting tokens found: {conflicts}")
+
+
+class DFANode(set[NFANode]):
+
+    def __init__(self, nfaNodes: set[NFANode]):
+        super().__init__(nfaNodes)
+        # Calculate the ϵ-closure of nfaNodes
+        stack = list(nfaNodes)
+        while stack:
+            t = stack.pop(0)
+            if '' not in t:
+                continue
+            for state in t['']:
+                if state not in self:
+                    self.add(state)
+                    stack.insert(0, state)
+
+    def move(self, to: str) -> "DFANode":
+        destNodes = set()
+        for node in self:
+            if to in node:
+                destNodes.update(node[to])
+        return DFANode(destNodes)
+
+    @property
+    def token(self) -> str:
+        tokens = [x.token for x in self if hasattr(x, 'token')]
+        if len(tokens) > 1:
+            raise DFAConflict(tokens)
+        if not tokens:
+            return False
+        return tokens[0]
+
+
+class DFA:
+    def __init__(self, nfa: NFA):
+        self.start = DFANode({nfa.start})
+        self.end = nfa.end
+
+    def match(self, string: str) -> tuple[int, str]:
+        state = self.start
+        i = 0
+        for i, c in enumerate(string):
+            nstate = state.move(c)
+            if not nstate:
+                break
+            state = nstate
+        else:
+            i += 1
+        if state.token:
+            return (state.token, i)
+        return False
---
+++ tests/grm/test_fsm.py
@@ -0,0 +1,110 @@
+# Copyright 2021 Ilango Rajagopal
+# Licensed under GPL-3.0-only
+
+import pytest
+from pypargen.grm import fsm
+
+
+def test_nfa_node_id():
+    a = fsm.NFANode()
+    b = fsm.NFANode()
+    c = fsm.NFANode()
+    d = fsm.NFANode()
+    assert a != b != c != d
+    assert a == a
+    assert b == b
+    assert c == c
+    assert d == d
+    assert hash(a) != hash(b) != hash(c) != hash(d)
+
+
+def test_nfa_node_transition():
+    start = fsm.NFANode()
+    start.add_transition('a', fsm.NFANode('atok'))
+    start.add_transition('b', fsm.NFANode())
+    start.add_transition('c', fsm.NFANode())
+    start.add_transition('c', fsm.NFANode())
+    start.add_transition('c', fsm.NFANode())
+    assert list(start.keys()) == list("abc")
+    assert start not in start['a']
+    assert start not in start['b']
+    assert start not in start['c']
+    assert len(start['c']) == 3
+    a = list(start['a'])[0]
+    assert hasattr(a, 'token')
+    assert a.token == 'atok'
+    assert not hasattr(list(start['b'])[0], 'token')
+
+
+@pytest.mark.xfail(strict=True, raises=AssertionError)
+def test_nfa_node_transition_invalid():
+    start = fsm.NFANode()
+    start.add_transition('hello', fsm.NFANode())
+
+
+def test_nfa():
+    nfa = fsm.NFA()
+    nfa.start.add_transition('a', nfa.end)
+    nfa.start.add_transition('b', nfa.end)
+    nfa.start.add_transition('c', nfa.end)
+    nfa.start.add_transition('c', nfa.end)
+    nfa.start.add_transition('c', nfa.end)
+
+    assert list(nfa.start.keys()) == list("abc")
+    assert nfa.end in nfa.start['a']
+    assert nfa.end in nfa.start['b']
+    assert nfa.end in nfa.start['c']
+    assert len(nfa.start['c']) == 1
+
+
+def test_dfa_node():
+    a = fsm.NFANode()
+    b = fsm.NFANode()
+    c = fsm.NFANode()
+    d = fsm.NFANode('atok')
+    a.add_transition('', b)
+    b.add_transition('', c)
+    c.add_transition('a', d)
+
+    dfanode = fsm.DFANode({a})
+    assert b in dfanode
+    assert c in dfanode
+    assert d not in dfanode
+    assert d in dfanode.move('a')
+    assert len(dfanode.move('a')) == 1
+    assert not dfanode.token
+    assert dfanode.move('a').token == "atok"
+
+
+@pytest.mark.xfail(strict=True, raises=fsm.DFAConflict)
+def test_dfa_node_conflict():
+    a = fsm.NFANode()
+    b = fsm.NFANode('b')
+    c = fsm.NFANode('c')
+    a.add_transition('a', b)
+    a.add_transition('a', c)
+
+    dfanode = fsm.DFANode({a})
+    dfanode.move('a').token
+
+
+def test_dfa():
+    nfa = fsm.NFA(end=fsm.NFANode("end"))
+    a = fsm.NFANode()
+    b = fsm.NFANode()
+    c = fsm.NFANode()
+    d = fsm.NFANode()
+
+    a.add_transition('', b)
+    b.add_transition('', c)
+    c.add_transition('a', d)
+
+    nfa.start.add_transition('a', a)
+    d.add_transition('d', nfa.end)
+    nfa.end.add_transition('a', d)
+
+    dfa = fsm.DFA(nfa)
+    assert dfa.match("aad") == ("end", 3)
+    assert dfa.match("aadaddd") == ("end", 5)
+    assert not dfa.match("aaa")
+    assert not dfa.match("a")
